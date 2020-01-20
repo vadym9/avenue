ed8c3bdc95a744edede31a7db632daf9ca0c9bf4
Vadym <vadym.putrov@chisw.com>
2020-01-20 09:46:48 +0200

    add carousel & jquery & update icomoon and clean

---
+++ src/js/slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider').slick({
+    //    dots:true
+    // prevArrow: $('.prev')
+    // infinite: true,
+    // slidesToShow:1,
+    // centerMode:true,
+    // centerMode:true,
+    // variableWidth:true
+    });
+});
